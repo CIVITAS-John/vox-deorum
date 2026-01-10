62a3cedaa0bc246e14f6004af845b9d1265d3232
John Chen <civitasjohn@gmail.com>
2026-01-09 23:42:21 -0700

    SQL fix.

--- civ5-mod/SQL/VoxDeorum_Options.sql
+++ civ5-mod/SQL/VoxDeorum_Options.sql
@@ -1,7 +1,7 @@
 UPDATE CustomModOptions	SET Value = 1 WHERE Name = 'IPC_CHANNEL';
-UPDATE CustomModOptions	SET Value = 1 WHERE Name like 'EVENTS_%'
+UPDATE CustomModOptions	SET Value = 1 WHERE Name like 'EVENTS_%';
 
 INSERT INTO Flavors
 	(Type)
 VALUES
-	('FLAVOR_MOBILIZATION'),
+	('FLAVOR_MOBILIZATION');
