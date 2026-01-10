d21142c6aa1335bded6a359c5bdf524fe0308326
John Chen <civitasjohn@gmail.com>
2026-01-10 10:59:19 -0700

    Fixed the localization.

---
+++ civ5-mod/Text/VoxDeorum_Text.sql
@@ -0,0 +1,7 @@
+UPDATE Language_en_US
+SET Text = 'The leader has expressed a public opinion to you.'
+WHERE Tag = 'TXT_KEY_SPECIFIC_DIPLO_STRING_1';
+
+UPDATE Language_en_US
+SET Text = 'The leader has formed a private opinion of you.'
+WHERE Tag = 'TXT_KEY_SPECIFIC_DIPLO_STRING_2';
