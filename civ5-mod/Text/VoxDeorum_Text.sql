25a96520718350396ffd57e23d17620dff763fda
John Chen <civitasjohn@gmail.com>
2026-01-11 21:18:12 -0700

    Updated text.

--- civ5-mod/Text/VoxDeorum_Text.sql
+++ civ5-mod/Text/VoxDeorum_Text.sql
@@ -2,4 +2,10 @@ INSERT OR REPLACE INTO Language_en_US (Tag, Text)
 VALUES ('TXT_KEY_SPECIFIC_DIPLO_STRING_1', 'The leader has expressed a public opinion to you (%d).');
 
 INSERT OR REPLACE INTO Language_en_US (Tag, Text)
+VALUES ('TXT_KEY_SPECIFIC_DIPLO_STRING_1_SELF', 'Our leader has expressed a public opinion to this player (%d).');
+
+INSERT OR REPLACE INTO Language_en_US (Tag, Text)
 VALUES ('TXT_KEY_SPECIFIC_DIPLO_STRING_2', 'The leader has formed a private opinion of you (%d).');
+
+INSERT OR REPLACE INTO Language_en_US (Tag, Text)
+VALUES ('TXT_KEY_SPECIFIC_DIPLO_STRING_2_SELF', 'Our leader has formed a private opinion to this player (%d).');
